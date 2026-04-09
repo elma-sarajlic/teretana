7ab8f6e8ddba0c3caffcfa554ad2ddf23b72850e
elma-sarajlic <budifiteu@gmail.com>
2026-04-10 00:54:25 +0200

    Update defaultExercises.js

--- src/data/defaultExercises.js
+++ src/data/defaultExercises.js
@@ -2,41 +2,48 @@ export const defaultExercises = [
   {
     id: 'e1',
     name: 'Čučnjevi (Squats)',
-    category: 'Noge',
-    description: 'Osnovna vježba za donji dio tijela. Držite leđa ravno i spuštajte se dok bedra ne budu paralelna s podom.',
-    videoUrl: 'https://player.vimeo.com/external/370331493.sd.mp4?s=7b34f6648ce273ba964e4604d7f9f30be9080b0c&profile_id=139&oauth2_token_id=57447761', // Sample squats
+    category: 'Donji dio',
+    description: 'Osnovna vježba za noge i gluteus. Stopala u širini ramena, leđa ravno.',
+    videoUrl: 'https://player.vimeo.com/external/370331493.sd.mp4?s=7b34f6648ce273ba964e4604d7f9f30be9080b0c&profile_id=139&oauth2_token_id=57447761',
     imageUrl: 'https://images.pexels.com/photos/3757334/pexels-photo-3757334.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
   {
     id: 'e2',
     name: 'Sklekovi (Pushups)',
-    category: 'Prsa / Ruke',
-    description: 'Vježba za gornji dio tijela. Aktivirajte core i spuštajte se prsima prema tlu.',
+    category: 'Gornji dio',
+    description: 'Vježba za prsa i tricepse. Tijelo u ravnoj liniji, laktovi pod 45 stepeni.',
     videoUrl: 'https://player.vimeo.com/external/494252666.sd.mp4?s=98d9e262489c62927236e7884d633215be11b22e&profile_id=139&oauth2_token_id=57447761',
     imageUrl: 'https://images.pexels.com/photos/176782/pexels-photo-176782.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
   {
     id: 'e3',
     name: 'Iskorak (Lunges)',
-    category: 'Noge / Gluteus',
-    description: 'Savršeno za oblikovanje nogu i gluteusa. Pazite da koljeno ne prelazi prste.',
+    category: 'Noge',
+    description: 'Savršeno za gluteus. Iskoračite naprijed tako da oba koljena tvore kut od 90 stupnjeva.',
     videoUrl: 'https://player.vimeo.com/external/403814894.sd.mp4?s=34017a4235288289899388f725a589255651c313&profile_id=139&oauth2_token_id=57447761',
     imageUrl: 'https://images.pexels.com/photos/3775164/pexels-photo-3775164.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
   {
     id: 'e4',
-    name: 'Plank',
-    category: 'Core',
-    description: 'Izometrička vježba za cijelo tijelo, posebno trbušne mišiće.',
-    videoUrl: 'https://player.vimeo.com/external/462550130.sd.mp4?s=40422c54f5934509744274c431d6837890ecce31&profile_id=139&oauth2_token_id=57447761',
-    imageUrl: 'https://images.pexels.com/photos/3823039/pexels-photo-3823039.jpeg?auto=compress&cs=tinysrgb&w=600'
+    name: 'Mrtvo Dizanje (Deadlift)',
+    category: 'Leđa / Zadnja loža',
+    description: 'Kraljica vježbi za snagu. Fokus na ravna leđa i potisak iz peta.',
+    videoUrl: 'https://player.vimeo.com/external/514457031.sd.mp4?s=69ed306788fa1dec3f7ce291c9693eb833f44359&profile_id=139&oauth2_token_id=57447761',
+    imageUrl: 'https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
   {
     id: 'e5',
-    name: 'Mrtvo dizanje (Deadlift)',
-    category: 'Leđa / Zadnja loža',
-    description: 'Kompleksna vježba za snagu cijelog tijela.',
-    videoUrl: 'https://player.vimeo.com/external/514457031.sd.mp4?s=69ed306788fa1dec3f7ce291c9693eb833f44359&profile_id=139&oauth2_token_id=57447761',
-    imageUrl: 'https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=600'
+    name: 'Rameni Potisak (Shoulder Press)',
+    category: 'Ramena',
+    description: 'Izgradnja stabilnosti ramena. Potisak bučicama iznad glave uz kontrolu.',
+    imageUrl: 'https://images.pexels.com/photos/3768593/pexels-photo-3768593.jpeg?auto=compress&cs=tinysrgb&w=600'
+  },
+  {
+    id: 'e6',
+    name: 'Plank (Izdržaj)',
+    category: 'Core',
+    description: 'Jačanje trbušnih mišića i stabilnosti kralježnice.',
+    videoUrl: 'https://player.vimeo.com/external/462550130.sd.mp4?s=40422c54f5934509744274c431d6837890ecce31&profile_id=139&oauth2_token_id=57447761',
+    imageUrl: 'https://images.pexels.com/photos/3823039/pexels-photo-3823039.jpeg?auto=compress&cs=tinysrgb&w=600'
   }
 ];
