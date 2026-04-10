f88c0c1d656d0de99b28056e5e648dd7aa22fe4f
elma-sarajlic <budifiteu@gmail.com>
2026-04-10 09:48:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Teretana/', // Matches the repository name for GitHub Pages
+  base: './', // Using relative paths makes it work on both root domains and subfolders like /Teretana/
 });
